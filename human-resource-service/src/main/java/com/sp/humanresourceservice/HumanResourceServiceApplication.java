ccc30ef3245dd2e39adba41971b9d69b9c08c7b6
TechSP <sp@example.com>
2022-01-23 16:29:16 +0530

    hrms service initial commit

---
+++ human-resource-service/src/main/java/com/sp/humanresourceservice/HumanResourceServiceApplication.java
@@ -0,0 +1,15 @@
+package com.sp.humanresourceservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class HumanResourceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HumanResourceServiceApplication.class, args);
+	}
+
+}
---
+++ human-resource-service/src/test/java/com/sp/humanresourceservice/HumanResourceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sp.humanresourceservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumanResourceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
