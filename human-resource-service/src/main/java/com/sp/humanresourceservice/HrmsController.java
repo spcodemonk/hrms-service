0d505c64016adb7699e9642dccd80f623ac20afd
TechSP <sp@example.com>
2022-01-24 20:24:11 +0530

    added hystrix in proejct

--- human-resource-service/src/main/java/com/sp/humanresourceservice/HrmsController.java
+++ human-resource-service/src/main/java/com/sp/humanresourceservice/HrmsController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 @RestController
 @RequestMapping("/hrms")
 public class HrmsController {
@@ -15,9 +17,18 @@ public class HrmsController {
 	EmployeeFeignClient employeeFeignCLient;
 
 	@GetMapping("/getTaxesPaid")
+	@HystrixCommand(fallbackMethod = "handleHystrixFallback")
 	public ResponseEntity<?> getEmployeeTax(){
 		
 		Object obj = employeeFeignCLient.sampleFeggnTest();
+		
+		ResponseEntity<?> entity = new ResponseEntity<>(obj, HttpStatus.ACCEPTED);
+		return entity;
+	}
+	
+public ResponseEntity<?> handleHystrixFallback(){
+		
+		Object obj = new String("Sharad");
 		
 		ResponseEntity<?> entity = new ResponseEntity<>(obj, HttpStatus.ACCEPTED);
 		return entity;
--- human-resource-service/src/main/java/com/sp/humanresourceservice/HumanResourceServiceApplication.java
+++ human-resource-service/src/main/java/com/sp/humanresourceservice/HumanResourceServiceApplication.java
@@ -2,12 +2,14 @@ package com.sp.humanresourceservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableEurekaClient
 @SpringBootApplication
 @EnableFeignClients
+@EnableCircuitBreaker
 public class HumanResourceServiceApplication {
 
 	public static void main(String[] args) {
